ccab65c9439bb85b14dfa2970e834ca5835f55a8
victorPonce <A00827302@tec.mx>
2023-06-01 13:31:05 -0600

    file uploaded and returns url

--- CBIpoc/ContentView.swift
+++ CBIpoc/ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 import PhotosUI
-import FirebaseFirestore
 import FirebaseStorage
+import FirebaseFirestore
 
 let db = Firestore.firestore()
-let storage = Storage.storage()
+
 struct ContentView: View {
     @State private var isCameraOpen = false
     @State private var isPhotoLibraryOpen = false
@@ -97,63 +97,34 @@ func uploadImage(_ image: UIImage) {
     let filename = UUID().uuidString + ".jpg"
     
     // Create a reference to the Firebase Storage location where the image will be uploaded
-    let storageRef = storage.reference()
+    let storageRef = Storage.storage().reference().child("\(filename)")
     
     // Upload the image data to Firebase Storage
-    print("before uploading the pictre")
-//    let uploadTask = storageRef.putData(imageData, metadata: nil) { (metadata, error) in
-//        if let error = error {
-//            print("Error uploading image: \(error.localizedDescription)")
-//            return
-//        }
-//
-//        // Metadata contains file metadata such as size, content-type.
-//        let size = metadata.size
-//        // You can also access to download URL after upload.
-//        storageRef.downloadURL { (url, error) in
-//            guard let downloadURL = url else {
-//                print("Error getting download URL: \(error.localizedDescription)")
-//                return
-//            }
-//            if let downloadURL = url {
-//                // You can save the downloadURL or perform any other operations with it
-//                print("Download URL: \(downloadURL)")
-//            }
-//        }
-//    }
-//
-//
-//    //    let uploadTask = storageRef.putData(imageData, metadata: nil) { (metadata, error) in
-//    //        if let error = error {
-//    //            print("Error uploading image: \(error.localizedDescription)")
-//    //            return
-//    //        }
-//    //
-//    //        // Image uploaded successfully
-//    //        print("Image uploaded.")
-//    //
-//    //        // Get the download URL of the uploaded image
-//    //        storageRef.downloadURL { (url, error) in
-//    //            if let error = error {
-//    //                print("Error getting download URL: \(error.localizedDescription)")
-//    //                return
-//    //            }
-//    //
-//    //            if let downloadURL = url {
-//    //                // You can save the downloadURL or perform any other operations with it
-//    //                print("Download URL: \(downloadURL)")
-//    //            }
-//    //        }
-//    //    }
-//
-//    // Observe the upload progress if needed
-//    uploadTask.observe(.progress) { snapshot in
-//        // Handle upload progress updates
-//        guard let progress = snapshot.progress else {
-//            return
-//        }
-//
-//        let percentComplete = Double(progress.completedUnitCount) / Double(progress.totalUnitCount) * 100.0
-//        print("Upload progress: \(percentComplete)%")
-//    }
+    let uploadTask = storageRef.putData(imageData, metadata: nil) { (metadata, error) in
+        if let error = error {
+            print("Error uploading image: \(error.localizedDescription)")
+            return
+        }
+        
+        // Image uploaded successfully
+        print("Image uploaded.")
+        storageRef.downloadURL { (url, error) in
+            if let error = error {
+                print("Error retrieving download URL: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let downloadURL = url else {
+                print("Download URL is nil.")
+                return
+            }
+            
+            // Use the downloadURL for further processing
+            print("Download URL: \(downloadURL)")
+            
+            
+        }
+        // Get the download URL of the uploaded image
+        
+    }
 }
