a74a7d01a08839293de973ca3813587180f4a1a9
victorPonce <A00827302@tec.mx>
2023-05-31 18:37:06 -0600

    init'

---
+++ CBIpoc/CBIpocApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBIpocApp.swift
+//  CBIpoc
+//
+//  Created by Victor Ponce on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CBIpocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
